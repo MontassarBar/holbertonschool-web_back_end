02ace0fb527ec256a52916ae40cc6d70615f78f7
MontassarBar <montassarbarbouchi7@gmail.com>
2022-09-19 18:40:42 +0200

    Duck typing - first element of a sequence

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''annotating a function'''
+from typing import Union,Sequence,  Any
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''annotation'''
+    if lst:
+        return lst[0]
+    else:
+        return None
