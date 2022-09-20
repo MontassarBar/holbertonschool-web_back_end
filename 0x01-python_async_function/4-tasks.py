3cff03ed7fb2e92557e07ff121fa0c31629a1157
MontassarBar <montassarbarbouchi7@gmail.com>
2022-09-20 20:05:43 +0200

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is
+being called'''
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    '''basic stuff'''
+    i = 0
+    list = []
+    for i in range(0, n):
+        r = await wait_random(max_delay)
+        list.append(r)
+    return sorted(list)
