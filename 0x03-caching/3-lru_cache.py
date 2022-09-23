cef038fa5ba0e16175628841ba11a66ef063f423
MontassarBar <montassarbarbouchi7@gmail.com>
2022-09-23 20:27:41 +0200

    LRU Caching

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -14,16 +14,16 @@ class LRUCache(BaseCaching):
         ''' assign to the dictionary self.cache_data
             the item value for the key'''
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            print("DISCARD:", self.rm[len(self.rm) - 1])
-            del(self.cache_data[self.rm[len(self.rm) - 1]])
-            del(self.rm[len(self.rm) - 1])
+            if key not in self.cache_data.keys():
+                print("DISCARD:", self.rm[len(self.rm) - 1])
+                del(self.cache_data[self.rm[len(self.rm) - 1]])
+                del(self.rm[len(self.rm) - 1])
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if key in self.cache_data.keys():
-                for k in range(0, len(self.rm) - 1):
-                    if self.rm[k] == key:
-                        del(self.rm[k])
-                self.rm.insert(0, key)
+            for k in range(0, len(self.rm) - 1):
+                if self.rm[k] == key:
+                    del(self.rm[k])
+            self.rm.insert(0, key)
 
     def get(self, key):
         '''return the value in self.cache_data linked to key'''
