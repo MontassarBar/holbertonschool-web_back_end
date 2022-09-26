87888fc83c0cddf1fd9b288fe87db3f86f9d8211
MontassarBar <montassarbarbouchi7@gmail.com>
2022-09-26 18:49:14 +0200

    Simple pagination

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page > 0
+        assert page_size > 0
+        list = []
+        x = self.index_range(page, page_size)
+        self.dataset()
+        if x[1] <= len(self.__dataset):
+            for i in range(x[0], x[1]):
+                list.append(self.__dataset[i])
+        return list
+
+    def index_range(self, page: int, page_size: int) -> Tuple:
+        return (page - 1) * page_size, page_size * page
