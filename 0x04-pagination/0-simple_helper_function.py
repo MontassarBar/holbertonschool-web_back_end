e608fd4772ec85b2ee59ba251d3ffcf37871f9f5
MontassarBar <montassarbarbouchi7@gmail.com>
2022-09-27 10:56:35 +0200

    Simple helper function

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
+'''index range'''
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple:
+    '''index range'''
     return (page - 1) * page_size, page_size * page
