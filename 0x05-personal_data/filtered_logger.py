04f3d9754a8c25921a816fa80a49646427450606
MontassarBar <montassarbarbouchi7@gmail.com>
2022-09-30 18:09:52 +0200

    Regex-ing

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ from typing import List
 
 
 def filter_datum(fields: List[
-                str], redaction: str, message: str, separator: str):
+                str], redaction: str, message: str, separator: str) -> str:
     '''returns the log message obfuscated'''
     for y in fields:
         message = re.sub(f"{y}=.+?{separator}",
