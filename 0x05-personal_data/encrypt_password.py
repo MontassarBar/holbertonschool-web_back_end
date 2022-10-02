3d79fc059f2621a3b0ced09f4bea7732bbc5639b
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-02 16:44:18 +0200

    Encrypting passwords

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''hash_password'''
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    '''hashing'''
+    bytes = password.encode('utf-8')
+    hash =  bcrypt.hashpw(bytes, bcrypt.gensalt())
+    return hash
