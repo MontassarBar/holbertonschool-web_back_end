3d562f10f5c83f8dab95e0cde889028c9158895a
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-04 17:22:59 +0200

    auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -7,11 +7,30 @@ from typing import List, TypeVar
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        '''require auth'''
+        '''public method require auth'''
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        else:
+            p = path
+            c = 0
+            if p[-1] == '/':
+                s = p.rstrip(p[-1])
+            else:
+                s = p
+            for x in excluded_paths:
+                if x[-1] == '/':
+                    p2 = x
+                    s2 = p2.rstrip(p2[-1])
+                else:
+                    s2 = p2
+                if s == s2:
+                    c += 1
+            if c == 0:
+                return True
         return False
 
     def authorization_header(self, request=None) -> str:
-        '''authorisation header'''
+        '''public method authorisation header'''
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
