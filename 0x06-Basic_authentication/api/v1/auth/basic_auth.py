ed732881478d1a4ad4a8d4591cb22fe846aff141
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-04 19:46:34 +0200

    BasicAuth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,21 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     '''empty'''
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        '''returns the Base64 part of the Authorization header'''
+        if authorization_header is None:
+            return None
+        if str(authorization_header).isnumeric() is True:
+            return None
+        x = ""
+        y = ""
+        for i in range(0, len(authorization_header)):
+            if i <= 5:
+                x += authorization_header[i]
+            if i > 5:
+                y += authorization_header[i]
+        if x != "Basic ":
+            return None
+        else:
+            return y
