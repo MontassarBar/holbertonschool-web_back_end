ff7caf95fd5ee5153443e33055b06bb91ed2fa57
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-04 19:09:21 +0200

    auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -32,7 +32,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         '''public method authorisation header'''
-        return None
+        if request is None:
+            return None
+        if not request.headers.get('Authorization'):
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''current user'''
