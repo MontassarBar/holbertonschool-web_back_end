e00dc6d974eaef4bf9d629c64958ca47b1284fb6
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-04 20:05:38 +0200

    BasicAuth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 '''BasicAuth class'''
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -24,3 +25,17 @@ class BasicAuth(Auth):
             return None
         else:
             return y
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        '''returns the decoded value of a Base64 string
+            base64_authorization_header'''
+        if base64_authorization_header is None:
+            return None
+        if str(base64_authorization_header).isnumeric() is True:
+            return None
+        try:
+            x = base64.b64decode(base64_authorization_header)
+            return x.decode('utf-8')
+        except Exception:
+            return None
