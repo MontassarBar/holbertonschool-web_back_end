6cf7236b7b92d727e096e3fd154a49852ddf1b6c
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-06 16:13:00 +0200

    app

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -36,11 +36,13 @@ def before_request():
     if x is False:
         pass
     else:
-        if auth.current_user(request) is None:
-            abort(403)
         if auth.authorization_header(
                 request) is None and auth.session_cookie(request) is None:
             abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+        if auth.authorization_header(request) is None:
+            return abort(401)
     request.current_user = auth.current_user(request)
 
 
