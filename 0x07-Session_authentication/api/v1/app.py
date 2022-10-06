08d3e7d7b68aef1dd8c96a54469f38d0ba59b8be
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-06 16:04:53 +0200

    app

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -31,7 +31,8 @@ def before_request():
         pass
     x = auth.require_auth(request.path, ['/api/v1/status/',
                                          '/api/v1/unauthorized/',
-                                         '/api/v1/forbidden/'])
+                                         '/api/v1/forbidden/',
+                                         '/api/v1/auth_session/login/'])
     if x is False:
         pass
     else:
@@ -39,6 +40,9 @@ def before_request():
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
+        if auth.authorization_header(
+                request) is None and auth.session_cookie(request) is None:
+            abort(401)
     request.current_user = auth.current_user(request)
 
 
