0538931a32b2ae496016c4a4e2760b6ce2ae3b50
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-12 15:00:06 +0200

    user

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+'''User model'''
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -7,6 +8,7 @@ Base = declarative_base()
 
 
 class User(Base):
+    '''User model'''
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
