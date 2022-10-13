c4c60676806d2c7842798b0c11b8aa12080aaecb
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-13 17:24:51 +0200

    auth file

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -79,3 +79,14 @@ class Auth:
             return user.reset_token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        '''hash the password and update the user’s hashed_password field
+            with the new hashed password and the reset_token field to None'''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id, hashed_password=_hash_password(
+                password), reset_token=None)
+            return None
+        except NoResultFound:
+            raise ValueError
