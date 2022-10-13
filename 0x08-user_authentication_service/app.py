b3c81cf7b3c4b973200ce1c9e0725a654f6338bc
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-13 16:28:27 +0200

    app file

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''flak app'''
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -25,6 +25,19 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password) is False:
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email)
+        resp = jsonify({"email": "<user email>", "message": "logged in"})
+        resp.set_cookie("session_id", session_id)
+        return resp
 
 
 if __name__ == "__main__":
