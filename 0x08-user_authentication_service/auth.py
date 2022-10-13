222eac6503c9313ff4283412041ef390b7447151
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-13 15:15:58 +0200

    auth file

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -28,3 +28,13 @@ class Auth:
             raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        ''' check the password with bcrypt.checkpw. If it matches return True.
+            In any other case, return False'''
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(
+                'utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
