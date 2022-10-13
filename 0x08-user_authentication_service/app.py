f966cb74a1045fd014384329d85a9aa14c0f4de1
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-13 15:02:59 +0200

    app file

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 '''flak app'''
+from auth import Auth
+from flask import Flask, jsonify, request
+from auth import Auth
 
-from flask import Flask, jsonify
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -9,6 +13,18 @@ app = Flask(__name__)
 def message():
     '''return a greeting message'''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    '''Register user'''
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
