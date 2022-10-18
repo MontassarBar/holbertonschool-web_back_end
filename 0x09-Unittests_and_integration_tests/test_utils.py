0eccc885aabcaf1b6c6817f83cb585284228ffac
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-18 16:14:40 +0200

    test_utils

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''unittest'''
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    '''TestAccessNestedMap class'''
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(
+            self, nested_map: Mapping, path: Sequence, value):
+        '''test that the method returns what it is supposed to'''
+        return self.assertEqual(access_nested_map(nested_map, path), value)
