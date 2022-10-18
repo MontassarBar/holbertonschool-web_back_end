1c8efef0c7374bba734c92bbbd46cf7ff570e6e1
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-18 17:20:08 +0200

    test_utils

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''unittest'''
 import unittest
+from unittest import mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from typing import (
     Mapping,
     Sequence,
@@ -10,6 +11,7 @@ from typing import (
     Dict,
     Callable,
 )
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -33,3 +35,19 @@ class TestAccessNestedMap(unittest.TestCase):
         '''test that a KeyError is raised'''
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''TestGetJson class'''
+    @parameterized.expand([
+        ("http://example.com", {"test_payload": True}),
+        ("http://holberton.io", {"test_payload": False})
+        ])
+    def test_get_json(self, url, test_payload):
+        '''Test that the output of get_json is equal to test_payload'''
+        return_value = mock.Mock()
+        return_value.json.return_value = test_payload
+        with patch(
+                "requests.get", return_value=return_value) as patched_requests:
+            self.assertEqual(get_json(url), test_payload)
+            patched_requests.assert_called_once()
