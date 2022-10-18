dc760db75f935b127c77ca09e851ea7d725e3692
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-18 17:42:09 +0200

    test_utils

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from unittest import mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from typing import (
     Mapping,
     Sequence,
@@ -51,3 +51,22 @@ class TestGetJson(unittest.TestCase):
                 "requests.get", return_value=return_value):
             self.assertEqual(get_json(url), test_payload)
             return_value.json.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    '''TestMemoize calss'''
+    def test_memoize(self):
+        '''Test that when calling a_property twice, the correct result is
+            returned but a_method is only called once'''
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass, 'a_method', return_value=42) as mock_method:
+            self.assertEqual(TestClass().a_property, 42)
+            mock_method.assert_called_once()
