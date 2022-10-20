387d6350d73f73b4bfb6264c3eb91aaa7c4019a3
MontassarBar <montassarbarbouchi7@gmail.com>
2022-10-20 16:58:32 +0200

    Parametrize templates

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -26,7 +26,7 @@ def get_locale():
 @app.route('/')
 def welcome():
     '''web page rendering'''
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
