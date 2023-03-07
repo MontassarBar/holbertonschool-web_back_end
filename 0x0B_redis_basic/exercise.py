dbc48519360b8d29f39fd31c1e8f025ea760b54b
MontassarBar <montassarbarbouchi7@gmail.com>
2023-03-07 13:16:04 +0100

    Writing strings to Redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -16,7 +16,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union(str, bytes, int, float)) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
         generate a random key, store the input data in Redis
         using the random key and return the key
