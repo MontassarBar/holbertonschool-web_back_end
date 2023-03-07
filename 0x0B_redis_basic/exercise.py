e7a08c20ef3406545a1ba59ec394041885bd55b6
MontassarBar <montassarbarbouchi7@gmail.com>
2023-03-07 14:25:47 +0100

    Redis exercise

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -24,3 +24,27 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Union[str, bytes, int, float]:
+        '''
+        get method that take a key string argument and an optional Callable
+        argument named fn. This callable will be used to convert the data back
+        to the desired format
+        '''
+        if fn:
+            return fn(self._redis.get(key))
+        else:
+            return self._redis.get(key)
+
+    def get_str(self, key: bytes) -> str:
+        '''
+        convert the key type to str
+        '''
+        return key.decode("utf-8")
+
+    def get_int(self, key: bytes) -> int:
+        '''
+        convert the key type to int
+        '''
+        return int(key)
