80bcc661faac773fe55927486b2edb1f4c91a969
MontassarBar <montassarbarbouchi7@gmail.com>
2023-03-16 18:15:11 +0100

    For...of Loops

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-	let array2 = []
-  for (let idx of array) {
-    	array2 = [...array2, appendString + idx]
+  let array2 = [];
+  for (const idx of array) {
+    array2 = [...array2, appendString + idx];
   }
-
   return array2;
 }
