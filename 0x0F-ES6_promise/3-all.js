864bf1025a55cdba851ce8c1b9a3a1eba64ff261
MontassarBar <montassarbarbouchi7@gmail.com>
2023-03-21 20:25:50 +0100

    Handle multiple successful promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { createUser, uploadPhoto } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((data) => console.log(data[0].body, data[1].firstName, data[1].lastName))
     .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
