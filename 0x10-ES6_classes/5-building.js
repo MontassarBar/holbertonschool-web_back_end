25cf707bf6f1dfcdccf8d27177438195642fab51
MontassarBar <montassarbarbouchi7@gmail.com>
2023-03-22 20:41:52 +0100

    A Building

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (!this.evacuationWarningMessage) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
