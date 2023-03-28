1011c7e24eba378f0db0cffcfe00424ea722650c
MontassarBar <montassarbarbouchi7@gmail.com>
2023-03-28 23:03:35 +0200

    Typed Arrays

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer);
   try {
+    const buffer = new ArrayBuffer(length);
+    const view = new DataView(buffer);
     view.setInt8(position, value);
-  } catch (Error) {
+    return view;
+  } catch (error) {
     throw Error('Position outside range');
   }
-  return view;
 }
