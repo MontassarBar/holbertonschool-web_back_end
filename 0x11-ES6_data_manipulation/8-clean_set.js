91a21a66f7b48e1a44f70b19eafe3975cda4c1a1
MontassarBar <montassarbarbouchi7@gmail.com>
2023-03-29 00:50:58 +0200

    Clean set

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,5 @@
 export default function cleanSet(set, startString) {
   let x = '';
-  if (startString === 'undefined') {
-    return x;
-  }
   for (const elem of set) {
     if (startString && elem && elem.substring(0, startString.length) === startString) {
       const str = elem.substring(startString.length, elem.lenght);
