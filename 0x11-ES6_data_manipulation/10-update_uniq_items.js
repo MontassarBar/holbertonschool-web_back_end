d057be2c3d77b3387f88a64fa8081a027067b3e0
MontassarBar <montassarbarbouchi7@gmail.com>
2023-03-29 00:49:43 +0200

    More map data structure

---
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItems(map) {
+  try {
+    map.forEach((value, key) => {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    });
+    return map;
+  } catch (error) {
+    throw Error('Cannot process');
+  }
+}
