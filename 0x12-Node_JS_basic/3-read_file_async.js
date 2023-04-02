d995467b08d6e4a45e9c71448d1ee08bcb14c05f
MontassarBar <montassarbarbouchi7@gmail.com>
2023-04-03 01:37:52 +0200

    Reading a file asynchronously with Node JS

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -7,36 +7,36 @@ async function countStudents(database) {
     fs.readFile(database, 'utf-8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
-        return;
-      }
-      const students = data.split('\n').map((student) => student.split(','));
-      header = students.shift();
-      for (let y = 0; y < students.length; y += 1) {
-        for (let x = 0; x < students[y].length; x += 1) {
-          students[y][header[x]] = students[y][x];
+      } else {
+        const students = data.split('\n').map((student) => student.split(','));
+        header = students.shift();
+        for (let y = 0; y < students.length; y += 1) {
+          for (let x = 0; x < students[y].length; x += 1) {
+            students[y][header[x]] = students[y][x];
+          }
+          students[y].splice(0, 4);
         }
-        students[y].splice(0, 4);
-      }
-      students.pop();
-      students.forEach((student) => {
-        if (fields.indexOf(student.field) === -1) {
-          fields.push(student.field);
+        students.pop();
+        students.forEach((student) => {
+          if (fields.indexOf(student.field) === -1) {
+            fields.push(student.field);
+          }
+        });
+        const arr = [];
+        arr.push(`Number of students: ${students.length}`);
+        console.log(`Number of students: ${students.length}`);
+        for (const field of fields) {
+          const n = students.filter((student) => student.field === field);
+          let firstnames = '';
+          for (const st of n) {
+            firstnames += `${st.firstname}, `;
+          }
+          arr.push(`Number of students in ${field}: ${n.length}. List: ${firstnames.slice(0, -2)}`);
+          console.log(`Number of students in ${field}: ${n.length}. List: ${firstnames.slice(0, -2)}`);
         }
-      });
-      const arr = [];
-      arr.push(`Number of students: ${students.length}`);
-      console.log(`Number of students: ${students.length}`);
-      for (const field of fields) {
-        const n = students.filter((student) => student.field === field);
-        let firstnames = '';
-        for (const st of n) {
-          firstnames += `${st.firstname}, `;
-        }
-        arr.push(`Number of students in ${field}: ${n.length}. List: ${firstnames.slice(0, -2)}`);
-        console.log(`Number of students in ${field}: ${n.length}. List: ${firstnames.slice(0, -2)}`);
+        const str = arr.join('\n');
+        resolve(str);
       }
-      const str = arr.join('\n');
-      resolve(str);
     });
   });
 }
