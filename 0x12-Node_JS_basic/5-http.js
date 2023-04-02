7353cb2f21a509da63084a532724dabf7d8dd3cc
MontassarBar <montassarbarbouchi7@gmail.com>
2023-04-02 22:31:03 +0200

    Create a more complex HTTP server using Node's HTTP module

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -4,20 +4,16 @@ const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   const urlParts = url.parse(req.url);
-  switch (urlParts.pathname) {
-    case '/':
-      res.write('Hello Holberton School!');
-      break;
-    case '/students':
-      countStudents(process.argv[2])
-        .then((msg) => {
-          res.write(`This is the list of our students ${msg}`);
-        });
-      break;
-    default:
-      res.write('Hello Holberton School!');
+  if (urlParts.pathname === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (urlParts.pathname === '/students') {
+    countStudents(process.argv[2])
+      .then((msg) => {
+        res.write(`This is the list of our students ${msg}`);
+      });
+    res.end();
   }
-  res.end();
 }).listen(1245);
 
 module.exports = app;
