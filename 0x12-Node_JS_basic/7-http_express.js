b4455a803b20af1f27947a7cc2d94dad4951d71e
MontassarBar <montassarbarbouchi7@gmail.com>
2023-04-03 01:31:43 +0200

    Create a more complex HTTP server using Express

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
+});
+app.get('/students', async (req, res) => {
+  countStudents(process.argv[2])
+    .then((result) => {
+      res.send(`This is the list of our students\n${result}`);
+    })
+    .catch((error) => {
+      res.send(error.message);
+    });
 });
 
 app.listen(port);
