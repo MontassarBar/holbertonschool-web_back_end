6e5588e30223d5c2f5ae8a9844defb56a7fd9276
MontassarBar <montassarbarbouchi7@gmail.com>
2023-04-04 00:10:37 +0200

    full server

--- 0x12-Node_JS_basic/full_server/routes/index.js
+++ 0x12-Node_JS_basic/full_server/routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const StudentsController = require('../controllers/StudentsController');
 
-const router = express.Router();
 function routes(app) {
+  const router = express.Router();
   app.use('/', router);
   router.get('/', (req, res) => {
     AppController.getHomepage(req, res);
