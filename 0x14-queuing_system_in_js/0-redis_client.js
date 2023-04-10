54da6043e0d8024788c0f18887406092027c82c7
MontassarBar <91077064+MontassarBar@users.noreply.github.com>
2023-04-11 01:47:06 +0200

    Node Redis Client

--- 0x14-queuing_system_in_js/0-redis_client.js
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -5,4 +5,3 @@ const client = createClient();
 async () => await client.connect();
 client.on('ready', () => {console.log('Redis client connected to the server')});
 client.on('error', err => console.log('Redis client not connected to the server:', err));
-
