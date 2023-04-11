b5ad1cba805b0c7bee6eeab6171233fe35fe7e1e
MontassarBar <montassarbarbouchi7@gmail.com>
2023-04-11 22:24:35 +0200

    Node Redis client and advanced operations

---
+++ 0x14-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,14 @@
+const redis = require('redis')
+const client = redis.createClient();
+
+async () => await client.connect();
+client.on('ready', () => console.log('Redis client connected to the server'));
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+
+client.hset('HolbertonSchools', 'Portland', '50', redis.print);
+client.hset('HolbertonSchools', 'Seattle', '80', redis.print);
+client.hset('HolbertonSchools', 'New York', '20', redis.print);
+client.hset('HolbertonSchools', 'Bogota', '50', redis.print);
+client.hset('HolbertonSchools', 'Cali', '40', redis.print);
+client.hset('HolbertonSchools', 'Paris', '2', redis.print);
+client.hgetall('HolbertonSchools', (err, res) => console.log(res));
