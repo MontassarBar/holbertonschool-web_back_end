d52addb678000f790a0ca78d66c50f2b744c692a
MontassarBar <montassarbarbouchi7@gmail.com>
2023-04-11 23:00:17 +0200

    subscriber

---
+++ 0x14-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,15 @@
+const redis = require('redis')
+const client = redis.createClient();
+
+async () => await client.connect();
+client.on('ready', () => console.log('Redis client connected to the server'));
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+
+client.subscribe('holberton school channel')
+client.on('message', (channel, message) => {
+    if (message === 'KILL_SERVER') {
+        client.unsubscribe();
+        client.quit();
+    }
+    console.log(message);
+  })
