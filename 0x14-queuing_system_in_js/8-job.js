acf6ca682cca3bf272dc9958a5e443e3afa69cb1
MontassarBar <montassarbarbouchi7@gmail.com>
2023-04-12 21:13:04 +0200

    Writing the job creation function

---
+++ 0x14-queuing_system_in_js/8-job.js
@@ -0,0 +1,14 @@
+export default function createPushNotificationsJobs(jobs, queue) {
+if (!Array.isArray(jobs)) {
+    throw(new Error('Jobs is not an array'));
+};
+for (const j of jobs) {
+const job = queue.create('push_notification_code_3', j).save(function(err){
+    if( !err ) console.log(`Notification job created: ${job.id}`);
+    });
+
+    job.on('progress', (progress, data) => console.log(`Notification job ${job.id} ${progress}% complete`));
+    job.on('complete', () => console.log(`Notification job ${job.id} completed`));
+    job.on('failed', (errorMessage) => console.log(`Notification job ${job.id} failed: ${errorMessage}`));
+};
+};
